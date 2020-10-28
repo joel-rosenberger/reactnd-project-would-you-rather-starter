d2495b7882e0965e70dc9b3e7db014af0176a933
Joel Rosenberger <joelrosenberger@gmail.com>
2020-10-27 21:36:43 -0400

    Show add new for empty question list

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { setQuestionFilter, QUESTION_FILTER_ANSWERED, QUESTION_FILTER_UNANSWERED } from '../actions/ui'
 import Question from './Question'
+import { Link } from 'react-router-dom'
+import NewQuestion from './NewQuestion'
 
 class Home extends Component {
 
@@ -21,15 +23,21 @@ class Home extends Component {
         ? filter = this.answered
         : filter = this.unanswered
 
+        let ids = this.props.questionIds.filter(id => filter(this.props.questions[id]))
+
         return <div class="panel panel-default">
             <ul class="nav nav-pills">
                 <li role="presentation" class={this.props.questionFilter===QUESTION_FILTER_ANSWERED?"active":""}><a name={QUESTION_FILTER_ANSWERED} onClick={this.handleFilterChange}>Answered</a></li>
                 <li role="presentation" class={this.props.questionFilter===QUESTION_FILTER_UNANSWERED?"active":""}><a name={QUESTION_FILTER_UNANSWERED} onClick={this.handleFilterChange}>Unanswered</a></li>
             </ul>
             <div class="panel-body">
-                {this.props.questionIds
-                .filter(id => filter(this.props.questions[id]))
-                .map(id => <Question questionId={id}></Question>)}
+                {ids.length > 0 
+                ? ids.map(id => <Question questionId={id}></Question>)
+                :<div>
+                    <h6>No unanswered questions found.</h6>
+                    <h5>Why don't you create a new question?</h5>
+                    <NewQuestion></NewQuestion>
+                </div>}
             </div>
         </div>
     }
